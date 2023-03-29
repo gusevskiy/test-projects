3db6f449eff45112c562d11db39d627ff8b2aed3
gusevskiy <vlgu@mail.ru>
2023-03-30 01:14:21 +0300

    Структурные директивы ngif, ngFor, *ngSwitchCase

--- src/app/components/foo/foo.component.ts
+++ src/app/components/foo/foo.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface User {
+  id: number;
+  name: string;
+  template: string;
+}
+
 @Component({
   selector: 'app-foo',
   templateUrl: './foo.component.html',
@@ -8,4 +14,36 @@ import { Component } from '@angular/core';
 export class FooComponent {
 
   expression = true;
+  deepObject: any = null;
+
+  users: User[] = [
+    {
+      id: 1,
+      name: 'John Doe',
+      template: 'bold'
+    },
+    {
+      id: 2,
+      name: 'Boris Britva',
+      template: 'italic'
+    },
+    {
+      id: 3,
+      name: 'Spanch Bob',
+      template: 'unknown'
+    }
+  ];
+
+  generateUser() {
+    this.deepObject = {
+      user: {
+        name: 'John',
+        suname: 'Duo'
+      }
+    };
+  }
+
+  trackBy(index: number, user: User) {
+    return user.id;
+  }
 }
